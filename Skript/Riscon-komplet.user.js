06d911048941e6abded843a900032eb10175d07c
Martin-CHT <m.orth@seznam.cz>
2026-05-05 21:31:51 +0200

    Edit

--- Skript/Riscon-komplet.user.js
+++ Skript/Riscon-komplet.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Riscon: Sdružené skripty
 // @namespace    https://github.com/Martin-CHT/Riscon
-// @version      9.0.18
+// @version      9.0.19
 // @description  Sdružený balík nástrojů pro Riscon. Modulární verze – každý modul je samostatný soubor načítaný přes @require.
 // @author       Martin
 // @copyright    2025-2026, Martin
